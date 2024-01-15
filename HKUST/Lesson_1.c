dc4a1d2158381758b877a6deec95de2fe9367657
mhr7dyn <139538957+mhr7dyn@users.noreply.github.com>
2024-01-15 13:50:43 +0000

    printing different data types

---
+++ HKUST/Lesson_1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{   
+    float weight = 7097070870.88888888888888888888f;
+    double dw = 7097070870.88888888888888888888;
+    printf("%g\n", weight);
+    printf("%f\n", weight);
+    printf("%lf\n", weight);
+    printf("%lf\n", dw);
+    printf("%.2f");
+}
